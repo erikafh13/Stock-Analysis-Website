a987055aa63f3463248bc243423d80290d886f0a
erikafh13 <127170590+erikafh13@users.noreply.github.com>
2026-06-18 16:10:38 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,4 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { fileURLToPath, URL } from 'node:url'
@@ -7,6 +8,15 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      // Saat dev lokal, /api/... diteruskan ke Vercel CLI (port 3000)
+      '/api': {
+        target: 'http://localhost:3000',
+        changeOrigin: true,
+      }
     }
   }
 })
